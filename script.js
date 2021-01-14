8e83279ad2d91d7b4f64fbc3b009d14c5ebdc3da
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-14 13:01:14 -0800

    finishes appendNumber function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
     this.previousOperandTextElement = previousOperandTextElement;
     this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
   }
 
   clear() {
@@ -13,6 +14,7 @@ class Calculator {
   delete() {}
 
   appendNumber(number) {
+    if (number === '.' && this.currentOperand.includes('.')) return;
     this.currentOperand = this.currentOperand.toString() + number.toString();
   }
 
