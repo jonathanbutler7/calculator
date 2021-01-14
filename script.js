9dd26c9d2536b4c5f4ec71aa1335b275efd058de
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-14 13:22:28 -0800

    finishes tutorial

--- script.js
+++ script.js
@@ -83,6 +83,8 @@ class Calculator {
       this.previousOperandTextElement.innerText = `${this.getDisplayNumber(
         this.previousOperand
       )} ${this.operation}`;
+    } else {
+      this.previousOperandTextElement.innerText = '';
     }
   }
 }
